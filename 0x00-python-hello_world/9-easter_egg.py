6404ec96cf172ccc9563491d3c03c7852887ef63
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-08 02:20:11 +0300

    0x00-python-hello_world: MODIFICATIONS TO TASK 9
    Now the length of the script is less than 98 characters

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-file_path = "The_Zen_of_Python.txt"
-with open(file_path, "r") as file:
-    content = file.read()
-    print(content)
+content = open("The_Zen_of_Python.txt", "r").read()
+print(content)
