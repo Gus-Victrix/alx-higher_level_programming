9ed39d441c36b99f1ce1fb5c251e2f558fd6b922
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-08 04:37:58 +0300

    0x00-python-hello_world: UPDATED HEADER FILE
    Removed redundunt code

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,8 +1,6 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stddef.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 /**
@@ -11,16 +9,17 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-void *_realloc(void *,int , int);
-void *_memcpy(void *,const void *, int);
 
-#endif
+#endif /* LISTS_H */
