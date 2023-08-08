877476afdfce07f17cd098cfddca4380bdd970c1
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-08 04:36:07 +0300

    0x00-python-hello_world: ADDED MORE FILES FOR TASK 10
    Attempting to improve score

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,102 +1,29 @@
 #include "lists.h"
 
-/**
- * check_cycle - checks for cycles in singly linked lists.
- *
- * @list: pointer to the head of the list.
- *
- * Return: 0 if none found, 1 if there is a cycle.
- */
-int check_cycle(listint_t *list)
-{
-	listint_t *current = list;
-	listint_t **arr = 0;
-	short n = 0;
-	short size = 0;
-	short i = 0;
 
-	if (list == NULL)
+/**
+* find_listint_loop - Find if the list contain infinite loop
+*
+* @head: linked list
+*
+* Return: The numer of elem
+*/
+int check_cycle(listint_t *head)
+{
+	listint_t *tortoise;
+	listint_t *hare;
+
+	if (head == NULL || head->next == NULL)
 		return (0);
-	
-	while (current != NULL)
+	tortoise = hare = head;
+	while (hare->next != NULL && hare->next->next != NULL)
 	{
-		if (n == size)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
 		{
-			size += 10;
-			arr = _realloc(arr, size * sizeof(listint_t **), (size - 10) *
-					sizeof(listint_t **));
+            return(1);
 		}
-		while (i < n)
-		{
-			if (arr[i++] == current)
-			{
-				free(arr);
-				return (1);
-			}
-		}
-		arr[i] = current;
-		current = current->next;
 	}
-	return (0);
+	return(0);
 }
-
-/**
- * _realloc - Reallocates Memory
- *
- * @ptr: Pointer to memory block that's to be reallocated.
- * @size: New size of allocated memory.
- * @old_size: Current size of malloc block.
- *
- * Return: Pointer to new memory block.
- */
-void *_realloc(void* ptr, int size, int old_size)
-{
-	void *new_ptr = NULL;
-
-    if (size == 0)
-	{
-        free(ptr);
-        return (NULL);
-    }
-
-    if (ptr == NULL)
-	{
-        return (malloc(size));
-    }
-
-    new_ptr = malloc(size);
-    if (new_ptr == NULL)
-	{
-        return (NULL); 
-    }
-
-    _memcpy(new_ptr, ptr, ((old_size < size) ? old_size : size) *
-			sizeof(listint_t **));
-
-    free(ptr);
-    return (new_ptr);
-}
-
-/**
- * _memcpy - copies memery content from one to the other
- *
- * @dest: Destination memeory.
- * @src: source of memery
- * @n: Size of memory block
- *
- * Return: destination address.
- */
-void* _memcpy(void* dest, const void* src, int n)
-{
-    unsigned char* dest_ptr = (unsigned char*)dest;
-    const unsigned char* src_ptr = (const unsigned char*)src;
-	int i = 0;
-
-    for (i = 0; i < n; i++)
-	{
-        dest_ptr[i] = src_ptr[i];
-    }
-
-    return (dest);
-}
-
