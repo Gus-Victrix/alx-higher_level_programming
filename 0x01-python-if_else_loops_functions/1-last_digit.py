cd41f9bd8f691b76f980a0bd037ec74f6f31c224
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-09 03:51:33 +0300

    0x01-python-if_else_loops_functions: FINAL MODIFICATIONS TO TASK 1
    Made to match pycode style guide

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,5 @@
 import random
 number = random.randint(-10000, 10000)
 a = number % 10 if number >= 0 else ((-number % 10) * (-1))
-b = "greater than 5" if a > 5 else ("0" if a == 0 else
-        "less than 6 and not 0")
+b = "greater than 5" if a > 5 else ("0" if a == 0 else "less than 6 and not 0")
 print(f"Last digit of {number:d} is {a:d} and is {b:s}")
