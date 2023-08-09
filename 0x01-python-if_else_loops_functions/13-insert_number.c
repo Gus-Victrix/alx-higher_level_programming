9b87c7b9d6bfced5d9ce03d1f3cb9528018e1127
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-09 05:34:46 +0300

    0x01-python-if_else_loops_functions: REMOVED REDUNDANT CODE IN TASK 13
    .

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,7 +12,6 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *current = *head;
-	int i = 0;
 
 	if (new_node == NULL)
 		return (NULL);
