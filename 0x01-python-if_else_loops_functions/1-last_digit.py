a5b1b588b094aff78c106256fc39a451f434a77d
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-09 03:42:20 +0300

    MODIFICAITONS TO TASK 1
    Removed redundant backslash and accounted for negative numbers and modified output of 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-a = number % 10
-b = "greater than 5" if a > 5 else ("is 0" if a == 0 else\
-        "is less than 6 and not 0")
+a = number % 10 if number >= 0 else -1 * (number % 10)
+b = "greater than 5" if a > 5 else ("0" if a == 0 else
+        "less than 6 and not 0")
 print(f"Last digit of {number:d} is {a:d} and is {b:s}")
