e7034afe295983c10bb479314e44aeebd172d5ae
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-09 05:43:51 +0300

    0x01-python-if_else_loops_functions: MODIFICATION TO TASK 13
    Solved the segfault

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,7 +18,7 @@ listint_t *insert_node(listint_t **head, int number)
 
 	new_node->n = number;
 
-	if (head == NULL || number < (*head)->n)
+	if (head == NULL || *head == NULL || number < (*head)->n)
 	{
 		new_node->next = *head;
 		*head = new_node;
