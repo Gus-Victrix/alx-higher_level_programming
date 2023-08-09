c545dad4c2f0e1ef40d410abae090570a5a10d47
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-09 04:14:04 +0300

    0x01-python-if_else_loops_functions: MODIFICATIONS TO TASK 6
    Now print uses .format instead of f-strings

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range(i + 1,10):
+    for j in range(i + 1, 10):
         if i == 8 and j == 9:
             break
-        print(f"{i}{j},", end=" ")
-print(f"{89:d}")
+        print("{:d}{:d},".format(i, j), end=" ")
+print("{:d}".format(89))
