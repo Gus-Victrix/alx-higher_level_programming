adfc17d883c8c01541ebba289eb1b3ae60f161e4
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-09 04:10:50 +0300

    0x01-python-if_else_loops_functions: MODIFICATIONS TO TASK 5
    Now print uses .format instead of f-strings

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for values in range(0,99):
-    print(f"{values:02d},", end=" ")
-print(f"{99:d}")
+for values in range(0, 99):
+    print("{:02d},".format(values), end=" ")
+print("{:d}".format(99))
