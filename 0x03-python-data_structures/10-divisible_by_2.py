39c89c61352208a4b1ed39c0230fc08f4c6581ca
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-14 23:21:26 +0300

    0x03-python-data_structures: MODIFICATION OF TASK 10
    Re-done the task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(my_list) == 0:
-        return
-    new_list
-
-    for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-
-    return new_list
+    return [True if i % 2 == 0 else False for i in my_list]
