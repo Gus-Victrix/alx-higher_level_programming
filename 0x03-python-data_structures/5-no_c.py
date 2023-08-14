0e7654b81c7f93b8b9ae6d3c685e458ef1c11447
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-14 22:35:17 +0300

    0x03-python-data_structures: MODIFICATION TO TASK 5
    Redone the task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    j = 0
-    while (j < len(my_string):
-            tmp = ord(my_string[j:j + 1])
-            if tmp != ord("c") and tmp != ord("C"):
-                new_string += my_string[j:j + 1]
-            j++
-    return new_string
+    return "".join([i for i in my_string if i != "c" and i != "C"])
