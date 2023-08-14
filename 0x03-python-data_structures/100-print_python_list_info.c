044336c3d359ce3c18fb8ec593a6e4cd49611036
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-14 23:56:32 +0300

    0x03-python-data_structures: MODIFICATION OF TASK 14
    Removed unused variable

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_python_list_info - Prints info about python lists
+ *
+ * @p: list to be checked
+ */
 void print_python_list_info(PyObject *p)
 {
-    PyListObject *list = (PyListObject *)p;
-    Py_ssize_t size = PyList_Size(p);
-    Py_ssize_t i;
-    PyListObject *listobj;
+	PyListObject *list = (PyListObject *)p;
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t i;
 
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
 
-    for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-        listobj = (PyListObject *)(list->ob_item[i]);
-        printf("Element %ld: %s\n", i, Py_TYPE(list->ob_item[i])->tp_name);
-    }
+		printf("Element %ld: %s\n", i, Py_TYPE(list->ob_item[i])->tp_name);
+	}
 }
