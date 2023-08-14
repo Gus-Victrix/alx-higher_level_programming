ab231d2b16aa0cbfea14c10465acadc0671a6bf0
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-14 23:41:31 +0300

    0x03-python-data_structures: ADDED TASK 14
    Initial commit of the task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_python_list_info(PyObject *p)
+{
+    PyListObject *list = (PyListObject *)p;
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t i;
+    PyListObject *listobj;
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", list->allocated);
+
+    for (i = 0; i < size; i++)
+	{
+        listobj = (PyListObject *)(list->ob_item[i]);
+        printf("Element %ld: %s\n", i, Py_TYPE(list->ob_item[i])->tp_name);
+    }
+}
