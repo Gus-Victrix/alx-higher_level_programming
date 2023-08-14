145726165581e4979ab04957ea5c6ff73e1a0654
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-14 22:47:09 +0300

    0x03-python-data_structures: MODIFICATION TO TASK 6
    Now the newline is printed as a string

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
             print("{:<2d}".format(element), end="")
-        print("{:s}".format(""))
+        print("{}".format("\n"), end="")
