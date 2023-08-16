184e213e5599e467916d75888ef63abef16fbfd8
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 16:02:14 +0300

    0x04-python-more_data_structures: MODIFICATIONS TO TASK 12
    Now checks the previous value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,13 @@ def roman_to_int(roman_string):
     Returns an integer value equivalent of input roman numeral.
     '''
     a = 0
+    prev = 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for n in reversed(roman_string):
-        if roman[n] < a:
+        if roman[n] < prev:
             a -= roman[n]
+            prev = roman[n]
         else:
             a += roman[n]
+            prev = roman[n]
     return a
