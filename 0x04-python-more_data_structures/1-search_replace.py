fb16dcba3bafd6684f55af18a73eca3ac96c34c2
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 13:18:21 +0300

    0x04-python-more_data_structures: CORRECTION OF TASK 1
    Modified comment to adhedere to pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@ def search_replace(my_list, search, replace):
     '''
     Replaces all occurences of an element with another in a new list.
 
-    >>> search_replace([1,2,3,4,5], 2, 89)
-    >>> [1,89,3,4,5]
+    >>> search_replace([1, 2, 3, 4, 5], 2, 89)
+    >>> [1, 89, 3, 4, 5]
     '''
     return [n if n != search else replace for n in my_list]
 
