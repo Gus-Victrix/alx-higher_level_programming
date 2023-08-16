338266047c657099c8fb15949d8de3b5c1b9af44
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 17:23:17 +0300

    0x04-python-more_data_structures: MODIFICATION TO TASK 15
    Modification to mechanism

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,9 @@ def complex_delete(a_dictionary, value):
     '''
     Deletes keys with the input value of the input dictionary.
     '''
-    if not a_dictionary:
-        return a_dictionary
-    a_dictionary = {k: v for k, v in a_dictionary.items() if v != value}
+    while a_dictionary:
+        for k, v in a_dictionary.items():
+            if v == value:
+                a_dictionary.pop(k)
+        break
     return a_dictionary
