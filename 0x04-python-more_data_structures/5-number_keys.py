81a9b1f1763e797d68ab46899cf05436dcef3488
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 13:50:31 +0300

    0x04-python-more_data_structures: ADDED TASK 5
    Initial commit of the task

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    '''
+    Returns the number of keys in a dictionary.
+
+    >>> number_keys({'1':1,'2':2, '3':3})
+    >>> 3
+    '''
+    return len(a_dictionary)
