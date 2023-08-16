84a5898ff236d57f5e4cc8daf05cc9bb997076a0
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 16:28:45 +0300

    0x04-python-more_data_structures: MODIFICATIONS TO TASK 13
    Added edgecase check

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,8 @@ def weight_average(my_list=[]):
     '''
     Returns the weighted average of all integers tuple (<score>, <weight>)
     '''
+    if not my_list:
+        return 0
     a = 0
     for score, weight in my_list:
         a += score * weight
