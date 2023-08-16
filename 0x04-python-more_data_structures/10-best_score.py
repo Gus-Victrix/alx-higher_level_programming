5754fe258f3d8ac94ae212ffbb2c838a91f6c234
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 15:07:51 +0300

    0x04-python-more_data_structures: ADDED TASK 10
    Initial commit of the task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    '''
+    Returns a key with the biggest integer value
+
+    >>> my_dict = {'a': 10, 'b': 20, 'c': 5}
+    >>> largest = best_score(my_dict)
+    >>> if largest:
+            print(f"The key with the largest value is '{largest_key}'")
+        else:
+            print("The dictionary is empty")
+
+    The key with the largest value is 'b'
+    '''
+    if not a_dictionary:
+        return None
+    return max(a_dictionary, key=dictionary.get)
