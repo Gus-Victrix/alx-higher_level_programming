21d3a674ece1a3f623b5fc0b8dcc296c073b0b9e
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 13:15:59 +0300

    0x04-python-more_data_structures: ADDED TASK 1
    Initial commit of the task

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    '''
+    Replaces all occurences of an element with another in a new list.
+
+    >>> search_replace([1,2,3,4,5], 2, 89)
+    >>> [1,89,3,4,5]
+    '''
+    return [n if n != search else replace for n in my_list]
+
