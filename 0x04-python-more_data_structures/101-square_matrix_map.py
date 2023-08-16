c6c2dd7ffc35852fa7716e176ef98b7a50a4abb6
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 16:52:44 +0300

    0x04-python-more_data_structures: MODIFICATION OF TASK 14
    Removed comments

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    '''
-    Returns the Hadamard square of a matrix
-    '''
     return list(map(lambda row: list(map(lambda a: a**2 , row)), matrix))
