62d2bf9bf95c63ae62fad74c9c2610e3c935ec94
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-16 16:14:43 +0300

    0x04-python-more_data_structures: MODIFICATIONS TO TASK 12
    Now the input is checked for type

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     '''
     Returns an integer value equivalent of input roman numeral.
     '''
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     a = 0
     prev = 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -13,4 +15,4 @@ def roman_to_int(roman_string):
         else:
             a += roman[n]
             prev = roman[n]
-    return a
+    return int(a)
