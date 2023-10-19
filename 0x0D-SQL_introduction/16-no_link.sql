75b1f2a28b43ab7122af64c16f5bb0f805226622
Gus-Victrix <iamfrsncis@gmail.com>
2023-10-19 03:25:51 +0300

    Fixed syntax error

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- Lists all records of the table where name is not empty.
-SELECT `score`, `name`, FROM `second_table`
+SELECT `score`, `name`
+FROM `second_table`
 WHERE `name` != ""
 ORDER BY `score` desc;
