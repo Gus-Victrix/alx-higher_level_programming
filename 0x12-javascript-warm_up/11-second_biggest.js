f0627736d420b06eb74f3756063871a5592f8e65
Gus-Victrix <iamfrsncis@gmail.com>
2023-11-14 01:53:44 +0300

    Semistandard linter related updates.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,12 @@ let largest = 0;
 let secondLargest = 0;
 
 args.forEach((element) => {
-	element = parseInt(element)
-	if (element > largest) {
-		secondLargest = largest;
-		largest = element;
-	} else if (element > secondLargest) {
-		secondLargest = element;
-	}
+  element = parseInt(element);
+  if (element > largest) {
+    secondLargest = largest;
+    largest = element;
+  } else if (element > secondLargest) {
+    secondLargest = element;
+  }
 });
 console.log(secondLargest);
