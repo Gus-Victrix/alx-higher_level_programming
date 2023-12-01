f004f48f6b7fbe8686c8e47e679017fa3c4b17de
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-12-01 18:35:28 +0000

    Update to task 6 of python_Networking#0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
-'''
-This module defines a function to find a peak in a list of unsorted ints.
-'''
+
+"""
+Interview questions peak
+"""
 
 
 def find_peak(list_of_integers):
-    '''
-    Finds peak in a list of ints using a linear search.
-
-    Args:
-        list_of_integers (list): The list of integers to be searched.
-
-    Raises:
-        TypeError: if input is not a list or a list of integers.
-
-    Returns: First peak found.
-    '''
-    if list_of_integers is None:
+    """
+    Find a peak in a list of numbers
+    args:
+        list_of_intergers (list): List of int
+    Return: None of not peak, the peak else
+    """
+    if len(list_of_integers) == 0:
         return None
-    if not isinstance(list_of_integers, list):
-        raise TypeError('Input must be a list of integers')
-    for i in range(len(nums)):
-        if (i == 0 or nums[i] >= nums[i - 1]) and (
-                i == len(nums) - 1 or nums[i] >= nums[i + 1]):
-            return nums[i]
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    return find_peak_rec(list_of_integers, 0, len(list_of_integers))
 
-    return None
+
+def find_peak_rec(nums, start, end):
+    """
+    Find a peak in a list of numbers via recurtions
+    args:
+        num (list): List of int
+        start (int): The idx start
+        end (int): The idx end
+    Return: None of not peak, the peak else
+    """
+    if start > end:
+        return None
+
+    middle = (start + end) // 2
+    if middle == 0 and nums[middle + 1] <= nums[middle]:
+        return nums[middle]
+    if middle == len(nums) - 1 and nums[middle - 1] <= nums[middle]:
+        return nums[middle]
+    if nums[middle - 1] < nums[middle] and nums[middle + 1] < nums[middle]:
+        return nums[middle]
+
+    if nums[middle - 1] > nums[middle]:
+        return find_peak_rec(nums, start, middle - 1)
+    else:
+        return find_peak_rec(nums, middle + 1, end)
