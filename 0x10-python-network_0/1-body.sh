99c89556bccc22b8c93ca93736ff83fcfa4a8bd6
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-12-01 18:29:01 +0000

    Update to task 1 of python-networking#0 project

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displaying body of Get response.
-curl -s -o /dev/null -w '%{http_code}' "$1" | { read -r status; [ "$status" -eq 200 ] && curl -s "$1"; }
+#Create a curl requests, and display the body responde
+curl -sL "$1"
