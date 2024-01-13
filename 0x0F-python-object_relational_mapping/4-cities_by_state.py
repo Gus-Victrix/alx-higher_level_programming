e9ca1e3eb480a3b730c998e8e2d13c5ba747165c
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-13 17:47:03 +0300

    :art: Making pycodestyle happy

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,7 +3,7 @@
 """
 Lists all cities from the database hbtn_0e_4_usa
 
-Usage: ./4-cities_by_state.py <mysql username>  <user password>  <database name>
+Usage:./4-cities_by_state.py <mysql username>  <user password>  <database name>
 """
 
 if __name__ == "__main__":
@@ -14,20 +14,20 @@ if __name__ == "__main__":
         print(__doc__)
         exit(1)
 
-    conn = ms.connect(  # Creating database connection
-        host="localhost", # Hostname
-        user=argv[1],     # Username
-        passwd=argv[2],   # Password
-        db=argv[3],       # Database name
-        port=3306)        # Port number
+    conn = ms.connect(     # Creating database connection
+        host="localhost",  # Hostname
+        user=argv[1],      # Username
+        passwd=argv[2],    # Password
+        db=argv[3],        # Database name
+        port=3306)         # Port number
 
-    cur = conn.cursor()  # Cursor object
+    cur = conn.cursor()    # Cursor object
 
     cur.execute(
-    "SELECT c.id, c.name, s.name\
-    FROM cities c JOIN states s\
-    ON c.state_id = s.state_id\
-    ORDER BY c.id ASC")  # Executing query
+        "SELECT c.id, c.name, s.name \
+         FROM cities c JOIN states s \
+         ON c.state_id = s.state_id \
+         ORDER BY c.id ASC")  # Executing query
 
     rows = cur.fetchall()  # Fetching all rows
 
