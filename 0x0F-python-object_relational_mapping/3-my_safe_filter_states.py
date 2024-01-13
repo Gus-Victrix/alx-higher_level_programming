b3bddb2c90656cc8bdccc2cafc96350ecd9911fa
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-13 16:46:59 +0300

    :memo: Redone with lots of comments

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
 
 """
-Safe the SQL injection from the last task
+Display all values in states table of hbtn_0e_0_usa where name matches argv[4].
+This time it's safe from SQL injections!
+
+Usage: ./0-select_states.py <username> <password> <database name> <state name>
 """
 
-import MySQLdb
-import sys
+if __name__ == "__main__":  # Execute only if run as script
+    import sys  # Exit error and commandline arguemnts access
+    import MySQLdb  # Connecting to mysql database.
 
-if "__main__" == __name__:
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8",
-    )
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER\
-            BY id ASC",
-        (sys.argv[4],),
-    )
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    if len(sys.argv) != 5:  # Check if the user input is valid
+        print(__doc__)  # Print the documentation
+        sys.exit(1)  # Exit with error
+
+    argv = sys.argv  # Assigning sys.argv to argv for easier access
+    if not argv[4].isalpha():  # Protecting against SQL injections
+        print("Argument 4 must be a string")
+        sys.exit(1)
+    conn = MySQLdb.connect(  # Connecting to MySQL database
+                username=argv[1],  # The database username.
+                passwd=argv[2],  # User password.
+                host="localhost",  # Location of the db
+                charset="utf8",  # Character set used in the database.
+                port=3306,  # Port to be used for connections
+                db=argv[3])  # Database to be used for connection.
+    cur = conn.cursor()  # Creating cursor object
+    # Execute the query to the database
+    rows = cur.execute("SELECT * FROM states WHERE name = '{}'\
+            ORDER BY id ASC".format(argv[4]))
+    for row in rows:  # Loop through all rows
+        if row[1] == argv[4]:  # Check if the row matches the state name
+            print(row)  # Print each row
+
+    cur.close()  # Close all cursors
+    conn.close()  # Close all databases
