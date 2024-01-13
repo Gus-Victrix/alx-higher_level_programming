822fe630222b97a1d83d93d9930b7dc70857b013
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-13 17:40:10 +0300

    :memo: Pointless comments added, oh and refactored too

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
 
 """
-List all the cities form the database
+Lists all cities from the database hbtn_0e_4_usa
+
+Usage: ./4-cities_by_state.py <mysql username>  <user password>  <database name>
 """
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    from sys import argv, exit
+    import MySQLdb as ms
 
-if "__main__" == __name__:
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8",
-    )
-    cur = conn.cursor()
+    if len(argv) != 4:
+        print(__doc__)
+        exit(1)
+
+    conn = ms.connect(  # Creating database connection
+        host="localhost", # Hostname
+        user=argv[1],     # Username
+        passwd=argv[2],   # Password
+        db=argv[3],       # Database name
+        port=3306)        # Port number
+
+    cur = conn.cursor()  # Cursor object
+
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM states JOIN\
-            cities ON cities.state_id = states.id ORDER BY cities.id ASC"
-    )
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    "SELECT c.id, c.name, s.name\
+    FROM cities c JOIN states s\
+    ON c.state_id = s.state_id\
+    ORDER BY c.id ASC")  # Executing query
+
+    rows = cur.fetchall()  # Fetching all rows
+
+    for row in rows:  # Iterating through the rows
+        print(row)    # Printing each row
+
+    cur.close()       # Closing cursor
+    conn.close()      # Closing connection
