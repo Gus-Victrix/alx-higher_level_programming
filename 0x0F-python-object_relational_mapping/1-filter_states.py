c2b0fc3b3331fc6405ec61ad1ddf5be8ee16ed92
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-13 13:59:37 +0300

    docs: Added lots of inline comments

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 
 """
-Select all state starting with the letter N
+Lists all states with a name starting with N from hbtn_0e_0_usa database.
+
+Usage: ./1-filter_states.py <mysql username, mysql password, database name>
 """
 
-import MySQLdb
-import sys
+if __name__ == "__main__":  # import guard
+    import MySQLdb  # import MySQLdb module for MySQL connections
+    import sys  # import sys module for command line arguments
 
-if "__main__" == __name__:
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8",
+    if len(sys.argv) != 4:  # check for valid number of arguments
+        print(__doc__)  # print module documentation
+        sys.exit(1)  # exit with error code 1 if conditions not met
+
+    conn = MySQLdb.connect(  # connect to database
+        host="localhost",  # host name
+        port=3306,  # port number
+        user=sys.argv[1],  # user supplied username
+        passwd=sys.argv[2],  # user supplied password
+        db=sys.argv[3],  # user supplied database name
+        charset="utf8",  # set encoding
     )
-    cur = conn.cursor()
+
+    cur = conn.cursor()  # create cursor object
+    # execute SQL query on cursor object
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if row[1][0] == 'N':
-            print(row)
-    cur.close()
-    conn.close()
+
+    rows = cur.fetchall()  # store query results in rows variable
+
+    for row in rows:  # iterate through rows variable
+        print(row)
+
+    cur.close()  # close cursor
+    conn.close()  # close connection to database
