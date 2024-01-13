b101c83fb7ee0faaa793db604a242f9d30e3a691
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-13 17:08:17 +0300

    :shield: SQL injection protection implemented.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,9 +16,6 @@ if __name__ == "__main__":  # Execute only if run as script
         sys.exit(1)  # Exit with error
 
     argv = sys.argv  # Assigning sys.argv to argv for easier access
-    if not argv[4].isalpha():  # Protecting against SQL injections
-        print("Argument 4 must be a string")
-        sys.exit(1)
     conn = MySQLdb.connect(  # Connecting to MySQL database
                 username=argv[1],  # The database username.
                 passwd=argv[2],  # User password.
@@ -28,12 +25,11 @@ if __name__ == "__main__":  # Execute only if run as script
                 db=argv[3])  # Database to be used for connection.
     cur = conn.cursor()  # Creating cursor object
     # Execute the query to the database
-    cur.execute("SELECT * FROM states WHERE name = '{}'\
-            ORDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s\
+            ORDER BY id ASC", (argv[4],))  # Defended from SQL injections
     rows = cur.fetchall()  # Fetch all the rows in a list
     for row in rows:  # Loop through all rows
-        if row[1] == argv[4]:  # Check if the row matches the state name
-            print(row)  # Print each row
+        print(row)  # Print each row
 
     cur.close()  # Close all cursors
     conn.close()  # Close all databases
