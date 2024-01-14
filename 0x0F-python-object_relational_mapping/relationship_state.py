40c9575378d42c625923454cdeed71fb287e1823
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 20:11:49 +0300

    :memo: Lots of comments incoming

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
+
 """
-contains the class definition of a State and an instance Base
+Define Class State that inherits from Base, a declarative base from SQLAlchemy.
+It also shows the relationship between this class and the City class.
+
+Attributes:
+-----------
+    id (int): The id of the state.
+    name (str): The name of the state.
 """
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship  # For creating relationships
+from sqlalchemy import Column, Integer, String, ForeignKey  # For db ops
+from sqlalchemy.ext.declarative import declarative_base  # For base class
 
-Base = declarative_base()
+Base = declarative_base()  # Create a declarative base class
 
 
 class State(Base):
-    """Representation of a state"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    """
+    Represents a state for a MySQL database.
+
+    This class maps to the table 'states' in the MySQL database 'hbtn_0e_6_usa'
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        id (sqlalchemy.Integer): The state's id.
+        name (sqlalchemy.String): The state's name.
+        cities (sqlalchemy.orm.relationship): The state's cities.
+    """
+    __tablename__ = "cities"  # The name of the MySQL table to store Cities.
+    id = Column(Integer, primary_key=True, autoincrement=True)  # The city's id
+    name = Column(String(128), nullable=False)  # The city's name
+    cities = relationship("City", back_populates="state")  # Easy state access
