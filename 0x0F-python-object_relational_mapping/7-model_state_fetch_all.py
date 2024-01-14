89a8e322ccec244d4304a5e5fe3cb21f68f05108
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 11:01:20 +0300

    :necktie: Altered output to match requred one.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -23,24 +23,19 @@ if __name__ == "__main__":  # Import guard.
 
     user, pw, db = argv[1], argv[2], argv[3]  # Spread argv into variables.
     engine = create_engine(  # Connecting to database.
-       f"mysql+mysqldb://{user}:{pw}@localhost:3306/{db}",  # Connection string.
+       f"mysql+mysqldb://{user}:{pw}@localhost:3306/{db}",  # Connecting to db.
        pool_pre_ping=True)  # Test connections before handing them out.
 
     Base.metadata.create_all(engine)  # Create metadata and tables.
 
-    Session = sessionmaker(  # Starting a db access session.
+    Session = sessionmaker(  # Starting a db access session class.
          bind=engine,  # Select the engine to be used for the session.
          expire_on_commit=True,  # Objects reloaded after every commit.
          autoflush=True,  # Sync with database on every query.
          autocommit=True)  # Every query is a transaction.
 
     session = Session()  # Create a Session instance.
-
-    stmt = (  # Create a statement to query the database.
-            session.query(State)  # Select all states.
-            .order_by(State.id.asc()))  # Order by id in ascending order.
-    states = stmt.all()  # Execute the statement and save the result.
-
+    states = session.query(State).order_by(State.id.asc())  # Query the table.
     for state in states:  # Iterate over the result.
         print(f"{state.id}: {state.name}")  # Print the result.
 
