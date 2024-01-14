2292d9f7bc09e975e9f9c35788d547956cf78a8b
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 13:45:07 +0300

    :memo: Added lots of comments into the code.

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,34 +1,36 @@
 #!/usr/bin/python3
 
 """
-Print the state object id
+Prints State object with name passed as argument from the hbtn_0e_6_usa db.
+
+Usage: ./test.py <mysql username> <mysql password> <database name> <state name>
+
+Arguments:
+    mysql username: username to connect the mySQL
+    mysql password: password to connect the mySQL
+    database name: name of the database
+    state name: name of the state to search
 """
 
-import sys
-from model_state import Base, State
+if __name__ == "__main__":  # Execute only if run as a script
+    from sys import argv, exit  # To collect user input and handle errors
+    from model_state import Base, State  # To access the State class
+    from sqlalchemy import create_engine  # To connect to the db
+    from sqlalchemy.orm import sessionmaker  # To communicate with the db
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+    user, pw, db, state = argv[1:]  # Unpack the arguments
+    engine = create_engine(  # Connect to the db
+        "mysql+mysqldb://{user}:{pw}@localhost/{db}",  # URI
+        pool_pre_ping=True)  # Test connections before handing them out
+    Base.metadata.create_all(engine)  # Create metadata tables in db
+    Session = sessionmaker(bind=engine)  # Create Session class
+    session = Session()  # Instantiate Session class
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
+    # Query the db for the State object with the name passed as argument
+    state = session.query(State).filter(State.name == state).first()
+    if state:  # If the state exists
+        print(state.id)  # Print the state id
+    else:  # If the state doesn't exist
+        print("Not found")  # Print an error message
 
-    session = Session(engine)
-    found = False
-    for state in (
-        session.query(State).filter(State.name == sys.argv[4]).
-        order_by(State.id)
-    ):
-        if state:
-            print(f"{state.id}")
-            found = True
-            break
-    if not found:
-        print("Not found")
-    session.close()
+    session.close()  # Close the connection to the db
