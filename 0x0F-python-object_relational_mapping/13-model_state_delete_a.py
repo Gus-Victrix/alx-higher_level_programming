a456e9b1efd679a97bea26d33ff8282861335526
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 17:21:19 +0300

    :memo: Added a very large number of inline comments.

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 
 """
-Delete state that contain a in their name
+Delete all State objects with name having 'a' in hbtn_0e_6_usa db.
+
+Usage: ./<file name> <mysql username> <mysql password> <database name>
+
+Arguments:
+    mysql username: username to connect the mySQL
+    mysql password: password to connect the mySQL
+    database name: name of the database
 """
 
 
-import sys
-from model_state import Base, State
+if __name__ == "__main__":  # Execute only if run as a script
+    from sys import argv, exit  # To collect user input and exit
+    from model_state import Base, State  # To access the State class
+    from sqlalchemy import create_engine  # To connect to the db
+    from sqlalchemy.orm import Session  # To communicate with the db
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+    if len(argv) != 4:  # If the number of arguments is not 4
+        print(__doc__)  # Print the documentation
+        exit(1)
+    user, pw, db = argv[1:]  # Unpack the arguments
+    engine = create_engine(  # Connect to the db
+        f"mysql+mysqldb://{user}:{pw}@localhost:3306/{db}",  # URI
+        pool_pre_ping=True)  # Test connections before handing them out
+    Base.metadata.create_all(engine)  # Create metadata tables in db
+    sess = Session(engine)  # Instantiate Session class
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    session.query(State).filter(State.name.like("%a%")).delete(
-        synchronize_session="fetch"
-    )
-    session.commit()
+    # Delete all State objects with name having 'a' in hbtn_0e_6_usa db
+    sess.query(State).filter(State.name.like('%a%')).delete(
+            synchronize_session='fetch')  # Syncing db and session
+    sess.commit()  # Save changes
+    sess.close()  # Close the connection to the db
