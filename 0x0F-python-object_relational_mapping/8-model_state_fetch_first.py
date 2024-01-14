59adb860b5b74e51670aef3bcc17f4086a2c14fe
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 11:25:05 +0300

    :art: Inline comments all over

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 
 """
-Print the first state of the database
+Display first State object from the database hbtn_0e_6_usa
+
+Usage: ./test.py <username> <password> <database name>
+
+Args:
+    username - username to connect the mySQL
+    password - password to connect the mySQL
+    database - Name of the database
 """
 
-import sys
-from model_state import Base, State
+if __name__ == "__main__":  # Import guard.
+    from sys import argv, exit  # For CLI arguments and error handling.
+    from sqlalchemy import create_engine  # To connect to the database.
+    from sqlalchemy.orm import Session  # To handle sessions.
+    from model_state import Base, State  # To access the table 'states'.
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+    if len(argv) != 4:
+        print(__doc__)
+        exit(1)
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
+    user, pw, db = argv[1:]  # Spread argv into variables.
 
-    session = Session(engine)
-    state = session.query(State).first()
-    if state:
+    engine = create_engine(  # Connecting to database.
+       f"mysql+mysqldb://{user}:{pw}@localhost:3306/{db}",  # Connecting to db.
+       pool_pre_ping=True)  # Test connections before handing them out.
+
+    Base.metadata.create_all(engine)  # Create metadata and tables.
+
+    session = Session(engine)  # Create a Session instance bound to engine.
+    state = session.query(State).order_by(State.id).first()  # Query the db.
+    if state:  # If the query returned a result.
         print(f"{state.id}: {state.name}")
-    else:
+    else:  # If the query returned nothing.
         print("Nothing")
-    session.close()
+    session.close()  # Close the session.
+    # session.commit(), session.expunge() are not needed due to configs.
