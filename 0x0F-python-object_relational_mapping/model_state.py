ccd6f88f6863951a4588c0e4de101a90a9683ef0
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 09:36:02 +0300

    :memo: Added documentation to the file.

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
 
 """
-Create the state class database
+Defines State class which inherits from Base.
+
+Base is a declarative base created by SQLAlchemy.
 """
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String  # For creating typed columns
 from sqlalchemy.ext.declarative import declarative_base
+# Making it possible to create classes that SQLAlchemy can manage.
 
-Base = declarative_base()
+Base = declarative_base()  # SQLAlchemy managable class.
 
 
-class State(Base):
-    """Class : State"""
+class State(Base):  # State class inherits from Base.
+    """
+    Defines State class which inherits from Base.
 
-    __tablename__ = "states"
-    id = Column(
-        Integer, autoincrement=True, unique=True,
-        nullable=False, primary_key=True
-    )
-    name = Column(String(128), nullable=False)
+    Attributes:
+        __tablename__ (str): Name of the table.
+        id (int): State id.
+        name (str): State name.
+    """
+    __tablename__ = 'states'  # Necessity for SQLAlchemy to work.
+    id = Column(  # Creating a column named id in the table.
+            Integer,  # Type of data that goes into the column.
+            primary_key=True,  # This will enforce uniqueness of column items.
+            unique=True,  # This will enforce uniqueness of column items.
+            autoincrement=True,  # The alchemy does the allocation job.
+            nullable=False)  # Ensures that the column is not empty.
+    name = Column(  # Creating a column named name in the table.
+            String(128),  # Column datatype and length specification.
+            nullable=False)  # Ensures that the column is not empty.
