8d0f4077c10cb770881f52a0a77cbe65d6163a09
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 16:49:25 +0300

    :memo: Added comments

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 
 """
-Add a new State
+Add State object with name "Louisiana"to hbtn_0e_6_usa db.
+
+Usage: ./<file name> <mysql username> <mysql password> <database name>
+
+Arguments:
+    mysql username: username to connect the mySQL
+    mysql password: password to connect the mySQL
+    database name: name of the database
 """
 
-import sys
-from model_state import Base, State
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+if __name__ == "__main__":  # Execute only if run as a script
+    from sys import argv, exit  # To collect user input and exit
+    from model_state import Base, State  # To access the State class
+    from sqlalchemy import create_engine  # To connect to the db
+    from sqlalchemy.orm import Session  # To communicate with the db
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
+    if len(argv) != 4:  # If the number of arguments is not 4
+        print(__doc__)  # Print the documentation
+        exit(1)
+    user, pw, db = argv[1:]  # Unpack the arguments
+    engine = create_engine(  # Connect to the db
+        f"mysql+mysqldb://{user}:{pw}@localhost:3306/{db}",  # URI
+        pool_pre_ping=True)  # Test connections before handing them out
+    Base.metadata.create_all(engine)  # Create metadata tables in db
+    sess = Session(engine)  # Instantiate Session class
 
-    session = Session(engine)
-    newState = State(name="Louisiana")
-    session.add(newState)
-
-    session.commit()
-    print(newState.id)
-    session.close()
+    state = State(name="Louisiana")  # Create State object
+    sess.add(state)  # Add State object to the db
+    sess.commit()  # Save changes
+    print(state.id)  # Print the id of the new State object
+    sess.close()  # Close the connection to the db
