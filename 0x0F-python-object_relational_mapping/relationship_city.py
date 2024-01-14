b518f5e4884e28c026cb56f5997bacf13c84f6a5
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 20:16:45 +0300

    :recycle: Reverted code to make it "unchanged" as required

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,19 +2,16 @@
 
 """
 Defines Class City that inherits from Base, a declarative base from SQLAlchemy.
-It also shows the relationship between this class and the State class.
 
 Attributes:
 -----------
     id (int): The id of the city.
     name (str): The name of the city.
     state_id (int): The id of the state that the city is in.
-    state (State): The state that the city is in.
 """
 
-from sqlalchemy.orm import relationship  # For creating relationships
 from sqlalchemy import Column, Integer, String, ForeignKey  # For db ops
-from relationship_state import Base, State  # Declarative base & state table
+from relationship_state import Base, State  # For declarative base and state
 
 
 class City(Base):
@@ -33,10 +30,4 @@ class City(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)  # The city's id
     name = Column(String(128), nullable=False)  # The city's name
     # The id of the city's state, must be an existing state id
-    state_id = Column(  # The state's id that the city is in
-        Integer,
-        ForeignKey('states.id'),  # Marking as a foreign key to states.id
-        nullable=False,  # Can't be empty
-        onupdate="CASCADE",  # If state id updates, update this too
-        ondelete="CASCADE")  # If state is deleted, delete this too
-    state = relationship("State", back_populates="cities")  # Easy state access
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
