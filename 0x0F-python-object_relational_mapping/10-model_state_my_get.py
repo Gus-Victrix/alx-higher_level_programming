43b5d7a5df7032e567f74dfcbacaebf92c553f6a
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-14 14:22:30 +0300

    :bulb: Refactored file to use Session instead of sessionmaker.

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,20 +16,19 @@ if __name__ == "__main__":  # Execute only if run as a script
     from sys import argv, exit  # To collect user input and handle errors
     from model_state import Base, State  # To access the State class
     from sqlalchemy import create_engine  # To connect to the db
-    from sqlalchemy.orm import sessionmaker  # To communicate with the db
+    from sqlalchemy.orm import Session  # To communicate with the db
 
     user, pw, db, state = argv[1:]  # Unpack the arguments
     engine = create_engine(  # Connect to the db
         "mysql+mysqldb://{user}:{pw}@localhost/{db}",  # URI
         pool_pre_ping=True)  # Test connections before handing them out
     Base.metadata.create_all(engine)  # Create metadata tables in db
-    Session = sessionmaker(bind=engine)  # Create Session class
-    session = Session()  # Instantiate Session class
+    session = Session(engine)  # Instantiate Session class
 
     # Query the db for the State object with the name passed as argument
     state = session.query(State).filter(State.name == state).first()
     if state:  # If the state exists
-        print(state.id)  # Print the state id
+        print(f"{state.id}")  # Print the state id
     else:  # If the state doesn't exist
         print("Not found")  # Print an error message
 
