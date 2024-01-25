b3278b797213e570330642f8ae583475850a31b2
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-25 23:03:45 +0300

    :sparkles: Update: Now the curl just prints the desired output and discards the query results.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Task still ambiguous
-curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
-
+# Prints "You got me!" after querying 0.0.0.0:5000/catch_me
+curl -s -w "You got me!"  0.0.0.0:5000/catch_me
