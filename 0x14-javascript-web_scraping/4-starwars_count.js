0df455efcdcd378512f13cf519bc256a557f53cf
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-20 21:02:00 +0300

    script: shows number of movies in which "Wedge Antilles" is present as a character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const URL = process.argv[2];
+
+async function getMethod (URL) {
+  await axios.get(`${URL}`)
+    .then((res) => {
+      let counter = 0;
+      for (const film of res.data.results) {
+        for (const listActors of film.characters) {
+          if (listActors.includes('people/18/')) {
+            counter++;
+          }
+        }
+      }
+      console.log(counter);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+getMethod(URL);
