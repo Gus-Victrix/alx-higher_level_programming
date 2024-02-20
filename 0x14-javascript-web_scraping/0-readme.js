f28553a650e3423b17bb5cedd9dda5544dcab85e
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-20 20:21:05 +0300

    linting: fixed semi-standard non-compliance

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 
-
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-	console.log(err);
+    console.log(err);
   } else {
-	console.log(data);
+    console.log(data);
   }
 });
