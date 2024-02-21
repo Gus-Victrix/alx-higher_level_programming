9e3a5e25e65fee60a15ce74459cd6f0e6b1da09c
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-21 05:44:00 +0300

    refactor: replaced axios module with request module

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
-
-const axios = require('axios');
-
+// A script that prints all characters of a Star Wars movie:
+const request = require('request');
 const id = process.argv[2];
-const URL = `https://swapi-api.hbtn.io/api/films/${id}`;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
-async function getMethod (URL) {
-  await axios.get(URL)
-    .then(async (res) => {
-      for (const characters of res.data.characters) {
-        await axios.get(characters)
-          .then((res) => {
-            console.log(res.data.name);
-          });
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const character = JSON.parse(body).characters;
+    printCharacters(character, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
-    }).catch((err) => {
-      console.log(`code: ${err.response.status}`);
-    });
+    }
+  });
 }
-
-getMethod(URL);
