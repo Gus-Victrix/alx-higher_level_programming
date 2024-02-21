920922f0ead1bc4579cae64113ea7b18aa01fbc3
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-21 04:35:44 +0300

    bugfix: changed output to print the expected format

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 const ep = process.argv[2];
-const URL = 'https://swapi-api.hbtn.io/api/films';
+const URL = 'https://swapi-api.hbtn.io/api/films/';
 
-async function getMethod (URL, ep) {
-  await axios.get(`${URL}/${ep}`)
-    .then((res) => {
-      console.log(`${res.data.title}`);
-    })
-    .catch((err) => {
-      console.error(`code :${err.response.status}`);
-    });
-}
-
-getMethod(URL, ep);
+request(URL + ep, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
